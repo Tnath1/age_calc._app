815cd4ce186261f4795de5e21815c07adbc2ada9
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2023-11-04 12:13:25 +0100

    handling errors

--- script.js
+++ script.js
@@ -6,26 +6,35 @@ document
     let day = document.querySelector("#days").value;
     let month = document.querySelector("#months").value;
     let year = document.querySelector("#years").value;
-    // Validate other fields as needed.
+
+    // Clear existing error messages
+    document.querySelectorAll(".error-message").forEach((errorElement) => {
+      errorElement.textContent = "";
+    });
 
     const errors = {};
 
     if (!day.trim()) {
       errors.day = "This field is required.";
+      document.querySelector("#days-error").textContent =
+        "This field is required.";
     }
 
     if (!month.trim()) {
       errors.month = "This field is required.";
+      document.querySelector("#months-error").textContent =
+        "This field is required.";
     }
     if (!year.trim()) {
       errors.year = "This field is required.";
+      document.querySelector("#years-error").textContent =
+        "This field is required.";
     }
 
     // Add more field validations as needed.
 
     if (Object.keys(errors).length > 0) {
       // Display error messages to the user
-      // You can add code here to display error messages near the fields or in a summary section.
     } else {
       // Form is valid, submit it to the server.
       document.getElementById("myForm").submit();
