0af265a0c41bd272b9fcc12d8f983d6def3b8ded
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2023-11-04 22:08:19 +0100

    added more error checking

--- script.js
+++ script.js
@@ -16,18 +16,30 @@ document.addEventListener("DOMContentLoaded", function () {
       errors.day = "This field is required.";
       document.querySelector("#days-error").textContent =
         "This field is required.";
+    } else if (isNaN(day) || day < 1 || day > 31) {
+      errors.day = "Please enter a valid day (1-31).";
+      document.querySelector("#days-error").textContent =
+        "Please enter a valid day.";
     }
 
     if (!month.trim()) {
       errors.month = "This field is required.";
       document.querySelector("#months-error").textContent =
         "This field is required.";
+    } else if (isNaN(month) || month < 1 || month > 12) {
+      errors.month = "Please enter a valid month (1-12).";
+      document.querySelector("#months-error").textContent =
+        "Please enter a valid month.";
     }
 
     if (!year.trim()) {
       errors.year = "This field is required.";
       document.querySelector("#years-error").textContent =
         "This field is required.";
+    } else if (isNaN(year) || year > new Date().getFullYear()) {
+      errors.year = "Year must be in the past.";
+      document.querySelector("#years-error").textContent =
+        "Year must be in the past.";
     }
 
     // Add more field validations as needed.
