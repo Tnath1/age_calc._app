8f0902c6f78f3bc42ebb6f4e35586a6bb18b7449
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2023-11-04 12:00:45 +0100

    added some html css & js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert("success");
+document
+  .querySelector(".container")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    let day = document.querySelector("#days").value;
+    let month = document.querySelector("#months").value;
+    let year = document.querySelector("#years").value;
+    // Validate other fields as needed.
+
+    const errors = {};
+
+    if (!day.trim()) {
+      errors.day = "This field is required.";
+    }
+
+    if (!month.trim()) {
+      errors.month = "This field is required.";
+    }
+    if (!year.trim()) {
+      errors.year = "This field is required.";
+    }
+
+    // Add more field validations as needed.
+
+    if (Object.keys(errors).length > 0) {
+      // Display error messages to the user
+      // You can add code here to display error messages near the fields or in a summary section.
+    } else {
+      // Form is valid, submit it to the server.
+      document.getElementById("myForm").submit();
+    }
+  });
